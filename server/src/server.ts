7e74896570520253c7cb57c8fc378de5a51cfeb7
Gin <76995293+devgin23@users.noreply.github.com>
2024-09-22 13:37:05 +0900

    개발 환경 구성 완료

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source"
 
 const app = express();
+
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -13,5 +15,8 @@ let port = 4000;
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
 
+    AppDataSource.initialize().then(async () => {
+        console.log("Database initialized!!")
+    }).catch(error => console.log(error))
 });
 
